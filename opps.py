f421dd213d1d46dd04dc1a70c7d99ff4acb14e11
Muhammad Arif <123452078+F22BSEEN1E02102@users.noreply.github.com>
2023-03-05 22:15:32 +0500

    Add files via upload

---
+++ opps.py
@@ -0,0 +1,31 @@
+# class student:
+#     def __init__(self ,name,age,Study):
+#         self.name=name
+#         self.age=age
+#         self.Study="Bs software Engeenring"
+#     # My_information="Introduction"
+        
+#     def info(self):
+#         print(f'Hello My Name is {self.name} and my age is {self.age} and I am the student of {self.Study}') 
+# a=student(1,2,3)
+# a.name="Arif nawaz"
+# a.age=20
+# s=a.info()
+# print(s)
+
+
+
+class Remote:
+    pass
+class player():
+    def moveright(self):
+        pass
+    def movetop(self):
+        pass
+    def moveleft(self):
+        pass
+Remote1=Remote()
+player1=player()
+if(Remote1.isLeftpreesed()):
+    player1.moveleft()
+
